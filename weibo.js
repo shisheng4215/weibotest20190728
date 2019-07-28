e39dfdacf1eb085eecbe4311ae7b7362ade77830
shisheng4215 <shisheng4215@163.com>
2019-07-28 21:50:00 +0800

    'init'

---
+++ weibo.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('port',process.env.PORT||3000);
+
+//定制404页面
+app.use(function(req,res) {
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+//定制500页面
+app.use(function(err,req,res,next) {
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'),function() {
+	console.log('=====  服务器已启动 ======== \n Express started on http://localhost:'+app.get('port'));
+});
